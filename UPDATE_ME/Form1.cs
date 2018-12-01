51184636bab58f66dda4da7b5739cb1b4bfdd0b4
Mehmet ELÇİÇİÇEKEKİNCİ <ekinci30@hotmail.com>
2018-12-01 04:39:39 +0300

    Proje dosyası ekle.

---
+++ UPDATE_ME/Form1.cs
@@ -0,0 +1,77 @@
+﻿using Ionic.Zip;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace UPDATE_ME
+{
+    public partial class Form1 : Form
+    {
+        string Yol = Application.StartupPath ;
+        string URLL = "https://github.com/mehmet02/mecesoft/raw/master/mece.zip";
+        string Dosyaadi = "mece.zip";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(Yol + @"\Temp"))
+            {
+                Directory.CreateDirectory(Yol + @"\Temp");
+            }
+            if (!File.Exists(Yol + @"\MeceSoft.exe"))
+            {
+
+            }
+            else
+            {
+                File.Copy("MeceSoft.exe", Yol + @"\Temp\MeceSoft.exe", true);
+            }
+        }
+        void ZipAc()
+        {
+            using (ZipFile zip1 = ZipFile.Read(Yol+"\\mece.zip"))
+            {
+                foreach (ZipEntry e in zip1)
+                {
+                    e.Extract(Yol + @"\", ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+        }
+        void Dosyaİndir(string Link, string Dizin, string DosyaAdı)
+        {
+            WebClient İstemci = new WebClient();
+            İstemci.DownloadFileCompleted += new AsyncCompletedEventHandler(DosyaİndirmeTamamlandığında);
+            İstemci.DownloadProgressChanged += new DownloadProgressChangedEventHandler(İndirmeİlerlemesi);
+            İstemci.DownloadFileAsync(new Uri(URLL), Dizin + @"\" + DosyaAdı);
+        }
+
+        void İndirmeİlerlemesi(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBarControl1.Position = (int)e.TotalBytesToReceive / 100;
+            progressBarControl1.Position = (int)e.BytesReceived / 100;
+        }
+
+        void DosyaİndirmeTamamlandığında(object sender, AsyncCompletedEventArgs e)
+        {
+            ZipAc();
+            MessageBox.Show("Program Başarıyla Güncellenmiştir.");
+            this.Close();
+        }
+        private void btnGuncel_Click(object sender, EventArgs e)
+        {
+            Dosyaİndir(URLL, Yol, Dosyaadi);
+            btnGuncel.Enabled = false;
+            btnKapat.Enabled = false;
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ git/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace UPDATE_ME
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBarControl1 = new DevExpress.XtraEditors.ProgressBarControl();
+            this.btnGuncel = new DevExpress.XtraEditors.SimpleButton();
+            this.btnKapat = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBarControl1
+            // 
+            this.progressBarControl1.Location = new System.Drawing.Point(3, 76);
+            this.progressBarControl1.Name = "progressBarControl1";
+            this.progressBarControl1.Properties.ShowTitle = true;
+            this.progressBarControl1.Size = new System.Drawing.Size(286, 32);
+            this.progressBarControl1.TabIndex = 2;
+            // 
+            // btnGuncel
+            // 
+            this.btnGuncel.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.btnGuncel.Appearance.Options.UseFont = true;
+            this.btnGuncel.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnGuncel.ImageOptions.Image")));
+            this.btnGuncel.Location = new System.Drawing.Point(3, 3);
+            this.btnGuncel.Name = "btnGuncel";
+            this.btnGuncel.Size = new System.Drawing.Size(140, 67);
+            this.btnGuncel.TabIndex = 4;
+            this.btnGuncel.Text = "Güncelle";
+            this.btnGuncel.Click += new System.EventHandler(this.btnGuncel_Click);
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Appearance.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.btnKapat.Appearance.Options.UseFont = true;
+            this.btnKapat.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnKapat.ImageOptions.Image")));
+            this.btnKapat.Location = new System.Drawing.Point(149, 3);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(140, 67);
+            this.btnKapat.TabIndex = 4;
+            this.btnKapat.Text = "Kapat";
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 110);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.btnGuncel);
+            this.Controls.Add(this.progressBarControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UPDATE ME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraEditors.ProgressBarControl progressBarControl1;
+        private DevExpress.XtraEditors.SimpleButton btnGuncel;
+        private DevExpress.XtraEditors.SimpleButton btnKapat;
+    }
+}
+
